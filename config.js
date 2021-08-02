271fde8785c0313ed24180ec5c4aa913a17e25d2
longgia0305 <88327859+longgia0305@users.noreply.github.com>
2021-08-02 20:47:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Lam iu ơi !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'moazzzzz😘😘',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'yêu long ',
+    btnNo: 'Không yêu long',
+    question:'Trên thế giới hơn 7 tỉ người thì lam yêu ai nhất',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu Long nhất mãi mãi iuuu Long😘😘',
+    mess: 'Mãi mãi iuuuuuuu lam . i love u 🥰🥰',
+    messDesc: 'Từ bây giờ không được bỏ tuiiii đâu nhó😘😘',
+    btnAccept: 'Vâng ạ🥰🥰. I LOVE U , TOO!',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
